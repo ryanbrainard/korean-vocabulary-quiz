6d6880ea08b8a517e1a5924d420b0e7edf64528c
Ryan Brainard <brainard@heroku.com>
2017-10-01 20:47:26 +0900

    Social styling

--- src/App.js
+++ src/App.js
@@ -8,16 +8,27 @@ class App extends Component {
   render() {
     return (
       <div>
+        <div className="a2a_kit a2a_kit_size_24 a2a_floating_style a2a_vertical_style" style={{right: '0px', top: '200px'}}>
+          <a className="a2a_button_facebook"></a>
+          <a className="a2a_button_twitter"></a>
+          <a className="a2a_button_reddit"></a>
+          <a className="a2a_dd" href="https://www.addtoany.com/share"></a>
+        </div>
+
         <Grid>
           <WordQuiz
             words={WORDS}
             sampleSize={50}
           />
+
           <footer>
             <hr/>
+            Made in Korea.
+            &nbsp;
             <a href="https://github.com/ryanbrainard/korean-vocabulary-quiz" target="_blank" rel="noopener noreferrer">
               View Source
             </a>
+            <hr/>
           </footer>
         </Grid>
       </div>
--- src/WordQuiz.js
+++ src/WordQuiz.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {PageHeader, Row, Col} from 'react-bootstrap';
+import {Jumbotron, Row, Col} from 'react-bootstrap';
 import {wordsPropType} from './words';
 import WordTable from './WordTable';
 
@@ -96,29 +96,29 @@ class WordQuiz extends Component {
     return (
       <div>
         <Row>
-          <Col md={10}>
-            <PageHeader>How Many Korean Words Do You Know?</PageHeader>
-            <p>
-              This is a quiz based on a random sample from the <a href="https://ko.wiktionary.org/wiki/%EB%B6%80%EB%A1%9D:%EC%9E%90%EC%A3%BC_%EC%93%B0%EC%9D%B4%EB%8A%94_%ED%95%9C%EA%B5%AD%EC%96%B4_%EB%82%B1%EB%A7%90_5800">list of the most common Korean words</a> published by the <a href="https://www.korean.go.kr/front_eng/main.do">The National Institute of The Korean Language</a>. For each word below, select if you know the word or not. Click on the word to see it in the dictionary to check yourself. See your result at the bottom.
-            </p>
-            <p>
-              The idea for this site was inspired by <a href="https://redd.it/72wf0s" target="_blank" rel="noopener noreferrer">this Reddit post</a>. Please note that this is mostly just for fun and probably not that accurate, but it should give Korean learners a ballpark figure of how many words they know. The accuracy is probably even less for very advanced learners or native speakers because they are likely to know all the words in this list and then some.
-            </p>
+          <Col xs={11} md={10}>
+            &nbsp;
+            <Jumbotron>
+              <h1>How Many Korean Words Do You Know?</h1>
+              <p>
+                This is a quiz based on a random sample from the <a href="https://ko.wiktionary.org/wiki/%EB%B6%80%EB%A1%9D:%EC%9E%90%EC%A3%BC_%EC%93%B0%EC%9D%B4%EB%8A%94_%ED%95%9C%EA%B5%AD%EC%96%B4_%EB%82%B1%EB%A7%90_5800">list of the most common Korean words</a> published by the <a href="https://www.korean.go.kr/front_eng/main.do">The National Institute of The Korean Language</a>. For each word below, select if you know the word or not. Click on the word to see it in the dictionary to check yourself. See your result at the bottom.
+              </p>
+              <p>
+                The idea for this site was inspired by <a href="https://redd.it/72wf0s" target="_blank" rel="noopener noreferrer">this Reddit post</a>. Please note that this is mostly just for fun and probably not that accurate, but it should give Korean learners a ballpark figure of how many words they know. The accuracy is probably even less for very advanced learners or native speakers because they are likely to know all the words in this list and then some.
+              </p>
+            </Jumbotron>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs={11} md={5}>
+            <WordTable words={sample}/>
             <hr/>
           </Col>
         </Row>
 
         <Row>
-          <Col sm={12} md={5}>
-            <WordTable
-              words={sample}
-            />
-            <hr/>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col md={10}>
+          <Col xs={10} md={10}>
             <h3>Results</h3>
 
             <p>
@@ -128,7 +128,8 @@ class WordQuiz extends Component {
             <h1>{results.allKnowCount} words</h1>
 
             <p>
-              The words are broken down into groups A, B, C from easiest to hardest, based on the sample above, here is how many you know in each group:
+              The words are broken down into groups A, B, C from easiest to hardest,
+              based on the sample above, here is how many you know in each group:
             </p>
 
             <ul>
@@ -138,8 +139,17 @@ class WordQuiz extends Component {
             </ul>
 
             <p>
-              Hover over the words to see their group designation. Refresh the page to try another random sample.
+              Hover over the words to see their group designation.
+              Refresh the page to try another random sample.
+              Share your results with a friend!
             </p>
+
+            <div class="a2a_kit a2a_kit_size_24 a2a_default_style">
+              <a className="a2a_button_facebook"></a>
+              <a className="a2a_button_twitter"></a>
+              <a className="a2a_button_reddit"></a>
+              <a className="a2a_dd" href="https://www.addtoany.com/share"></a>
+            </div>
           </Col>
         </Row>
       </div>
