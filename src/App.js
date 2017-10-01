c14e2fb2c5f12c6089c00762ce14ea78d677248c
Ryan Brainard <brainard@heroku.com>
2017-10-01 16:42:43 +0900

    Clean up text

--- src/App.js
+++ src/App.js
@@ -2,30 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import {WORDS} from './words';
 import WordQuiz from './WordQuiz';
-import { Grid, Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Grid } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar inverse fixedTop>
-          <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">Korean Vocabulary Quiz</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-            <Nav pullRight>
-              <NavItem href="https://github.com/ryanbrainard/korean-vocabulary-quiz" target="_blank">Source</NavItem>
-            </Nav>
-          </Grid>
-        </Navbar>
         <Grid>
           <WordQuiz
             words={WORDS}
             sampleSize={50}
           />
+          <footer>
+            <hr/>
+            <a href="https://github.com/ryanbrainard/korean-vocabulary-quiz" target="_blank" rel="noopener noreferrer">
+              View Source
+            </a>
+          </footer>
         </Grid>
       </div>
     );
--- src/WordQuiz.js
+++ src/WordQuiz.js
@@ -99,23 +99,21 @@ class WordQuiz extends Component {
           <Col md={10}>
             <PageHeader>How Many Korean Words Do You Know?</PageHeader>
             <p>
-              This is a quiz based on a random sample from the <a
-              href="https://ko.wiktionary.org/wiki/%EB%B6%80%EB%A1%9D:%EC%9E%90%EC%A3%BC_%EC%93%B0%EC%9D%B4%EB%8A%94_%ED%95%9C%EA%B5%AD%EC%96%B4_%EB%82%B1%EB%A7%90_5800">list
-              of the most common Korean words</a> published by the <a href="https://www.korean.go.kr/front_eng/main.do">The
-              National Institute of The Korean Language</a>. For each word below, select if you know the word or not.
-              See your result at the bottom. The idea for this site was inspired by <a href="https://redd.it/72wf0s"
-                                                                                       target="_blank"
-                                                                                       rel="noopener noreferrer">this
-              Reddit post</a>.
+              This is a quiz based on a random sample from the <a href="https://ko.wiktionary.org/wiki/%EB%B6%80%EB%A1%9D:%EC%9E%90%EC%A3%BC_%EC%93%B0%EC%9D%B4%EB%8A%94_%ED%95%9C%EA%B5%AD%EC%96%B4_%EB%82%B1%EB%A7%90_5800">list of the most common Korean words</a> published by the <a href="https://www.korean.go.kr/front_eng/main.do">The National Institute of The Korean Language</a>. For each word below, select if you know the word or not. Click on the word to see it in the dictionary to check yourself. See your result at the bottom.
             </p>
+            <p>
+              The idea for this site was inspired by <a href="https://redd.it/72wf0s" target="_blank" rel="noopener noreferrer">this Reddit post</a>. Please note that this is mostly just for fun and probably not that accurate, but it should give Korean learners a ballpark figure of how many words they know. The accuracy is probably even less for very advanced learners or native speakers because they are likely to know all the words in this list and then some.
+            </p>
+            <hr/>
           </Col>
         </Row>
 
         <Row>
-          <Col md={6}>
+          <Col sm={12} md={5}>
             <WordTable
               words={sample}
             />
+            <hr/>
           </Col>
         </Row>
 
@@ -123,14 +121,25 @@ class WordQuiz extends Component {
           <Col md={10}>
             <h3>Results</h3>
 
-            With some super fuzzy, not-so-scientific logic, that means you know approximately:
+            <p>
+              With some super fuzzy, not-so-scientific logic, that means you know approximately:
+            </p>
+
             <h1>{results.allKnowCount} words</h1>
+
+            <p>
+              The words are broken down into groups A, B, C from easiest to hardest, based on the sample above, here is how many you know in each group:
+            </p>
 
             <ul>
               {resultBreakdown('A')}
               {resultBreakdown('B')}
               {resultBreakdown('C')}
             </ul>
+
+            <p>
+              Hover over the words to see their group designation. Refresh the page to try another random sample.
+            </p>
           </Col>
         </Row>
       </div>
